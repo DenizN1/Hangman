32d6d9cdf47bbcdd013000c7c4deb46953568f0d
avdiji <favdiji@googlemail.com>
2023-05-27 23:24:19 +0200

    initial commit for hangman container

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Layout from "./components/layout/Layout";
 import { getRandomGermanWord } from "./words";
 import { useState } from "react";
 import HiddenWord from "./components/hiddenWord/HiddenWord";
+import HangmanContainer from "./components/hangmanContainer/HangmanContainer";
 
 export default function App() {
   const [getCurrentWord] = useState(getRandomGermanWord());
   const [getCurrentLetter, setCurrentLetter] = useState("");
+  const [getWrongAttempts, setWrongAttempts] = useState(0);
 
   return (
     <Router>
       <Layout>
+        <HangmanContainer/>
+        {/* <img src={process.env.PUBLIC_URL + "/images/hangman1.png"} alt="" /> */}
         <div>{getCurrentWord}</div>
         <HiddenWord
           getCurrentWord={getCurrentWord}
           getCurrentLetter={getCurrentLetter}
         />
-        {/* <img src={process.env.PUBLIC_URL + "/images/hangman1.png"} alt="" /> */}
         <AlphaButtons setCurrentLetter={setCurrentLetter} />
       </Layout>
     </Router>
---
+++ src/components/hangmanContainer/HangmanContainer.js
@@ -0,0 +1,5 @@
+import classes from "./HangmanContainer.module.css";
+
+export default function HangmanContainer(props){
+    return <div>Hangman Container</div>;
+}
