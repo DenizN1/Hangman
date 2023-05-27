c7e5db3bd02af61517b4f0671df6353167cdbe5e
avdiji <favdiji@googlemail.com>
2023-05-27 12:42:43 +0200

    added refresh button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import AlphaButtons from "./components/alphaButtons/AlphaButtons";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import { getRandomGermanWord } from "./words";
 
 export default function App() {
+  const currentWord = getRandomGermanWord();
+
   return (
-    <Layout>
-      <img src={process.env.PUBLIC_URL + "/images/hangman1.png"} alt=""/>
-      <AlphaButtons />
-    </Layout>
+    <Router>
+      <Layout>
+        <div>{getRandomGermanWord()}</div>
+        <img src={process.env.PUBLIC_URL + "/images/hangman1.png"} alt="" />
+        <AlphaButtons />
+      </Layout>
+    </Router>
   );
 }
-
-
-// process.env.PUBLIC_URL +
